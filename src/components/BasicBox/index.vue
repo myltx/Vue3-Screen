b5fd214f6ba90c1619868f72f3c07e84397119d8
myltx <summmayl@gmail.com>
2024-06-20 17:55:24 +0800

    feat: 添加BasicBox组件

---
+++ src/components/BasicBox/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  const props = defineProps({
+    title: {
+      type: [String, Number],
+      default: '标题',
+    },
+  });
+  const { title } = toRaw(props);
+</script>
+<template>
+  <div class="basic-box">
+    <div class="box-title">
+      <div class="title">{{ title }}</div>
+    </div>
+    <div class="box-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  @import './index.scss';
+  .basic-box {
+    width: 100%;
+    height: 100%;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    .box-title {
+      width: 100%;
+      height: 52px;
+      background: url('@/assets/images/basic/box/basic-title-bg.png') no-repeat;
+      background-size: 100% 100%;
+      display: flex;
+      align-items: center;
+
+      .title {
+        font-family: PangMenZhengDao;
+        font-size: 20px;
+        color: #ffffff;
+        letter-spacing: 1px;
+        text-align: left;
+        font-style: normal;
+        background: linear-gradient(top, #ffffff, #caf9ff);
+        margin-bottom: 20px;
+        padding: 20px;
+      }
+    }
+    .box-content {
+      color: #fff;
+      padding: 10px 20px;
+    }
+  }
+</style>
--- src/views/cockpit/index.vue
+++ src/views/cockpit/index.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts"></script>
 <template>
-  <PageWrapper :title="'大屏示例页面'"> 12313 </PageWrapper>
+  <PageWrapper :title="'大屏示例页面'">
+    <div class="container-left">
+      <BasicBox :title="'消防安全制度建设'"> 1 </BasicBox>
+      <BasicBox :title="'消防安全制度建设'" />
+      <BasicBox :title="'消防安全制度建设'" />
+      <BasicBox :title="'消防安全制度建设'" />
+    </div>
+  </PageWrapper>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .container-left {
+    width: 391px;
+    height: 255px;
+    background: linear-gradient(180deg, rgba(0, 0, 1, 0.5) 0%, #091829 100%);
+    backdrop-filter: blur(4px);
+  }
+</style>
