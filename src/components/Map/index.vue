f2a6b73471d337bfa4a25c847db7bfa000746436
myltx <summmayl@gmail.com>
2024-07-30 09:34:29 +0800

    fix: 去除天空颜色

--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-  import { BMap, BZoom } from 'vue3-baidu-map-gl';
+  import { BMap, BMarker } from 'vue3-baidu-map-gl';
 </script>
 <template>
   <BMap
+    :heading="64.5"
+    :tilt="50"
     :height="'100%'"
     class="map"
     ak="0BMG1CekNJ2VVFRrrmX6x6qma8WHYGY0"
     mapStyleId="6053418609ae26d7c32f05d45ea7991b"
   >
-    <BZoom />
+    <BMarker :zIndex="99" :position="{ lat: 39.915185, lng: 116.403901 }" icon="start" />
   </BMap>
 </template>
 
