82ef8717dd278909c29f8e32d0d8008a6e623762
myltx <summmayl@gmail.com>
2024-08-02 09:37:13 +0800

    fix: 解决冲突问题

--- src/views/cockpit/components/Bottom.vue
+++ src/views/cockpit/components/Bottom.vue
@@ -3,11 +3,7 @@
 <template>
   <div class="container-bottom position-bottom-0 z-999 flex">
     <div class="w-50% mr-10px bg-#112034">
-<<<<<<< HEAD
-      <BasicBox :title="'消防安全制度建设'" :size="'large'">12321321</BasicBox>
-=======
       <BasicBox :title="'消防安全制度建设'" :size="'large'" :height="'200px'">21313</BasicBox>
->>>>>>> 90494fda88e83544b0bd70f102278e3933f36881
     </div>
     <div class="w-50% bg-#112034">
       <BasicBox :title="'消防安全制度建设'" :width="'50%'">21313</BasicBox>
