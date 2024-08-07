07ee1d1abd9a00c7290a5e90c5add63c7647fd28
panjiawen <1125452700@qq.com>
2024-08-07 09:47:54 +0800

    feat: 机构侧-弹窗

--- src/views/institution/components/xfDetailModel.vue
+++ src/views/institution/components/xfDetailModel.vue
@@ -27,9 +27,27 @@ const handleClose = () => {
         <BasicModal v-model:modalValue="openModal" :title="'消防安全培训详情'" @closed="handleClose">
             <div class="ml-38px mr-38px main-container">
                 <div class="catagory">
-                    <span>
-                        消防演练
-                    </span>
+                    <span>消防演练</span>
+                </div>
+                <div class="title ml-8px">四月消防安全培训</div>
+                <div class="mt-20px detail-container">
+                    <div>
+                        <span class="tip">时间：</span>
+                        <span class="time ml-8px">2023-11-02 16:42:45</span>
+                    </div>
+                    <div class="mt-8px">
+                        <span class="tip">地点：</span>
+                        <span class="time ml-8px">老人活动室</span>
+                    </div>
+                    <div class="mt-8px flex">
+                        <span class="fj">附件：</span>
+                        <div class="flex img-container">
+                            <img src="@/assets/images/institution/video.png" alt="" width="25%" />
+                            <img src="@/assets/images/institution/video.png" alt="" width="25%" />
+                            <img src="@/assets/images/institution/video.png" alt="" width="25%" />
+                            <img src="@/assets/images/institution/video.png" alt="" width="25%" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </BasicModal>
