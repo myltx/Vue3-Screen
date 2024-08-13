5544ec025bf9f378909eefd4127ccda34439b977
myltx <summmayl@gmail.com>
2024-08-13 15:52:58 +0800

    fix: #ZHXF-844

--- src/components/Empty/index.vue
+++ src/components/Empty/index.vue
@@ -8,8 +8,8 @@
 </script>
 
 <template>
-  <div class="w-100%">
-    <img src="@/assets/images/basic/empty.png" alt="" class="h-80px w-120px mx-auto" />
+  <div class="w-100% h-100% flex flex-col justify-center items-center">
+    <img src="@/assets/images/basic/empty.png" alt="" class="h-80px w-120px" />
     <div class="text-center w-100% text-#999 text-20px mt-10px">{{ props.text }}</div>
   </div>
 </template>
